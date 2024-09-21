2a8e2c4326857a4f65209f51dbac1af4228a9b53
Ryan Fleishman <61712104+InTheFleish@users.noreply.github.com>
2024-09-21 16:48:25 -0400

    shaker
    it shakes. add to main camera if it's not when you download it

---
+++ Assets/Scripts/Shaker.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    public bool start = false;
+    public AnimationCurve curve;
+    public float duration = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                start = false;
+                StartCoroutine(Shaking());
+            }
+        }
+
+    }
+
+    IEnumerator Shaking()
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / duration);
+            transform.position = startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        transform.position = startPosition;
+    }
+}
